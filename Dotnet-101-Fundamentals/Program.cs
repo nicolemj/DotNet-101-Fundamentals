7fc3d8661fbd8d43bc7f87b341506a079130a748
Jeffries <NJeffries@geico.com>
2018-07-02 08:15:58 -0400

    Add project files.

---
+++ Dotnet-101-Fundamentals/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_101_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
